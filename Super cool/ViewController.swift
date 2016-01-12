48d200b052b8d4386a8864f2ef71eb67dd4fa2cd
Josh C <legomania@cox.net>
2016-01-12 06:09:45 -0800

    Completed app!
    the app is finished…. for now…..

--- Super cool/ViewController.swift
+++ Super cool/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Background: UIImageView!
+    
+    @IBOutlet weak var Logo: UIImageView!
+    
+    @IBOutlet weak var uncoolbutton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func uncoolbuttonACTION(sender: AnyObject) {
+        Background.hidden = false
+        Logo.hidden = false
+        uncoolbutton.hidden = true
+    }
 
 }
 
